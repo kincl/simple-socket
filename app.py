e1d27c3f7ecc57f531136c89158206e3b42cd120
Jason Kincl <kincljc@ornl.gov>
2016-12-09 14:55:04 -0500

    adding while loop for infinite daemon

--- app.py
+++ app.py
@@ -11,11 +11,15 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
-conn, addr = s.accept()
-print 'Connection address:', addr
-while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    print "received data:", data
-    conn.send(data)  # echo
-conn.close()
+while True:
+    # wait for connection
+    conn, addr = s.accept()
+    try:
+        print 'Connection address:', addr
+        while 1:
+            data = conn.recv(BUFFER_SIZE)
+            if not data: break
+            print "received data:", data
+            conn.send(data)  # echo
+    finally:
+        conn.close()
