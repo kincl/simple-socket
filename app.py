f0e4387782050443f78c889bacfdd4119825925f
Jason Kincl <kincljc@ornl.gov>
2016-12-09 15:20:11 -0500

    fixing socket

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import sys
 import socket
 
 TCP_IP = ''
-TCP_PORT = 5006
+TCP_PORT = 5005
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
